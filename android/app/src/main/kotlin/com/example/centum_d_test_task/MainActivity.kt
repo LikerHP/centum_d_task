1633ee43e1366d795e524673a1df2c90e7c032fd
LikerHP <d.shatilov52@gmail.com>
2025-03-12 12:40:40 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/centum_d_test_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.centum_d_test_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:centum_d_test_task/app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
